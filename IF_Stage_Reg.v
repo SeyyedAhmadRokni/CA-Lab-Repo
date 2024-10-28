ef2e243e96a7165302606e4da742326cc2e65b2b
SeyyedAhmadRokni <93092785+SeyyedAhmadRokni@users.noreply.github.com>
2024-10-28 10:38:53 +0330

    IF_Stage_Reg file updated

--- IF_Stage_Reg.v
+++ IF_Stage_Reg.v
@@ -1,7 +1,14 @@
 module IF_Stage_Reg(
     input clk, rst, freeze, flush,
     input [31:0] PC_in, Instruction_in,
-    output reg [31:0] PC,Instruction
+    output reg [31:0] PC, Instruction
 );
+    always@(posedge clk,posedge rst) begin
+        if (rst == 1'b1)
+            PC = 32'b0;
+        else
+            PC <= PC_in;
+            Instruction <= Instruction_in;
+    end
 
 endmodule
