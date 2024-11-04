9786f16d649e981822917c3f064f0d8336589a31
SeyyedAhmadRokni <93092785+SeyyedAhmadRokni@users.noreply.github.com>
2024-11-04 10:32:32 +0330

    register file added

---
+++ Registerfile.v
@@ -0,0 +1,27 @@
+module Registerfile(
+    input clk, rst,
+    input [3:0] src1, src2, Dest_wb,
+    input [31:0] Result_WB,
+    input writeBackEn,
+    output [31:0] reg1, reg2
+);
+    reg [31:0] regmem [3:0];
+
+    integer i;
+    assign reg1 = regmem[src1];
+    assign reg2 = regmem[src2];
+    always @(negedge clk, posedge rst) begin
+        if (rst)begin
+            for (i = 0; i < 16; i = i + 1) begin
+                regmem[i] = i;
+            end
+            regmem[0] = 32'b0;
+        end
+        else
+            regmem [Dest_wb] = Result_WB;
+
+
+    end
+
+
+endmodule
