4f84ff6e4412a621d9819ac875ac159124d358a0
SeyyedAhmadRokni <93092785+SeyyedAhmadRokni@users.noreply.github.com>
2024-11-11 13:43:49 +0330

    some bug fixed in arm.v and cpu.v

--- ARM.v
+++ ARM.v
@@ -303,6 +303,6 @@ input          TD_CLK27;            //	TV Decoder 27MHz CLK
 inout	[35:0]	GPIO_0;					//	GPIO Connection 0
 inout	[35:0]	GPIO_1;					//	GPIO Connection 1
 	
-	CPU cpu(.clk(CLOCK_50), .rst(SW[0]));
+	CPU cpu(.clk(CLOCK_50), .rst(SW[0]), .PC(LEDR[3:0]));
 	
 endmodule
