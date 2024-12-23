4358b62acfb7059f3190db36d34c073ddaf2bc0b
SeyyedAhmadRokni <93092785+SeyyedAhmadRokni@users.noreply.github.com>
2024-12-23 15:25:39 +0330

    mem_stage outputs : ready, SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N; should connect
    must write a test for sram

--- src/MEM_Stage.v
+++ src/MEM_Stage.v
@@ -15,11 +15,27 @@ module MEM_Stage(clk, rst, ALU_ResIn, MEM_W_ENIn, MEM_R_ENIn, WB_ENIn,
     output wire[N - 1:0] DataMemoryOut, ALU_ResOut, MEM_EX_ALU_ResOut;
 
 
-    DataMemory DM(
-        .clk(clk), .rst(rst), .ALU_ResIn(ALU_ResIn),
-        .Value_RmIn(Value_RmIn), .MEM_W_ENIn(MEM_W_ENIn), 
-        .MEM_R_ENIn(MEM_R_ENIn), .resultOut(DataMemoryOut)
+    // DataMemory DM(
+    //     .clk(clk), .rst(rst), .ALU_ResIn(ALU_ResIn),
+    //     .Value_RmIn(Value_RmIn), .MEM_W_ENIn(MEM_W_ENIn), 
+    //     .MEM_R_ENIn(MEM_R_ENIn), .resultOut(DataMemoryOut)
+    // );
+    
+    output ready;
+    wire [15:0] SRAM_DQ;
+    wire [17:0] SRAM_ADDR;
+    output SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N;
+
+    Sram_Controller sram_controller(
+        .clk(clk), .rst(rst), .wr_en(MEM_W_ENIn), .rd_en(MEM_R_ENIn),
+        .address(ALU_ResIn), .writeData(Value_RmIn), .readData(DataMemoryOut), .ready(ready),
+        .SRAM_DQ(SRAM_DQ), .SRAM_ADDR(SRAM_ADDR),
+        .SRAM_UB_N(SRAM_UB_N), .SRAM_LB_N(SRAM_LB_N), .SRAM_WE_N(SRAM_WE_N), .SRAM_CE_N(SRAM_CE_N), .SRAM_OE_N(SRAM_OE_N)
     );
+
+    SRAM sram(
+        .clk(clk), .rst(rst), .SRAM_WE_NIn(SRAM_ADDR), .SRAM_ADDRIn(ALU_ResIn), .SRAM_DQInOut(SRAM_DQ)
+    )
 
     assign MEM_R_ENOut = MEM_R_ENIn;
     assign WB_ENOut = WB_ENIn;
