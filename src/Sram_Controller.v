3336bccaa2468565032876c07f5fff5cd1ce299d
SeyyedAhmadRokni <93092785+SeyyedAhmadRokni@users.noreply.github.com>
2024-12-23 15:07:45 +0330

    sram controller inshaallah

---
+++ src/Sram_Controller.v
@@ -0,0 +1,81 @@
+module Sram_Controller (
+     clk, rst, wr_en, rd_en,
+     address, writeData, readData, ready,
+     SRAM_DQ, SRAM_ADDR,
+     SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N
+);
+    input clk, rst, wr_en, rd_en,
+    input [31:0] address, writeData;
+    output [31:0] readData;
+    output ready;
+    
+    inout [15:0] SRAM_DQ;
+    output [17:0] SRAM_ADDR;
+    output SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N;
+
+    reg [2:0] ps, ns;
+
+    always (rst, posedge clk)begin
+        if (rst)
+            ps <= 3'd0;
+        else
+            ps <= ns;
+    end
+    
+    always @(posedge clk) begin
+        case (ps)
+            3'd0: ns <= (wr_en || rd_en) ? 3'd1 : 3'd0;
+            3'd1: ns <= 3'd2;
+            3'd2: ns <= 3'd3;
+            3'd3: ns <= 3'd4;
+            3'd4: ns <= 3'd5;
+            3'd5: ns <= 3'd0;
+        endcase
+
+    end
+
+    always @(wr_en, rd_en) begin
+        {SRAM_UB_N, SRAM_LB_N, SRAM_WE_N, SRAM_CE_N, SRAM_OE_N} = 1;
+        ready = 1'b0;
+        if (wr_en)
+            SRAM_DQ = 16'bz;
+
+        3'd0: begin
+            ready = ~(wr_en | rd_en);
+        end
+        3'd1: begin
+            SRAM_WE_N = 1'b0;
+            SRAM_LB_N = 1'b0;
+            SRAM_ADDR = address[18:1];
+            if (wr_en)
+                SRAM_DQ = writeData[15:0];
+
+        end
+        3'd2: begin
+            SRAM_WE_N = 1'b0;
+            SRAM_UB_N = 1'b0;
+            SRAM_ADDR = SRAM_ADDR + 1;
+            if (wr_en)
+                SRAM_DQ = writeData[31:16];
+            else if (rd_en)
+                readData[15:0] = SRAM_DQ;
+
+        end
+        3'd3: begin
+            if (rd_en)
+                readData[31:16] = SRAM_DQ;
+
+
+        end
+        3'd4: begin
+
+        end
+        3'd5: begin
+            ready = 1'b1;
+        end
+
+    end
+
+
+
+endmodule
