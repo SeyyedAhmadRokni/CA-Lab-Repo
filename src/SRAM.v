405c5e8ac8dc2212c9753a304eb19e7a637eb7f9
AmirhosseinRHT <amirhosseinrahati@gmail.com>
2025-01-06 11:32:31 +0330

    Updated SRAM module

--- src/SRAM.v
+++ src/SRAM.v
@@ -3,14 +3,12 @@ module SRAM(clk, rst, SRAM_WE_NIn, SRAM_ADDRIn, SRAM_DQInOut);
     input SRAM_WE_NIn;
     input [17:0] SRAM_ADDRIn;
     inout [15:0] SRAM_DQInOut;
-
-    // reg [15:0] memory [0:511];
-    reg [15:0] memory [0:1023];
+    reg [15:0] memory [0:511];
+    
     assign SRAM_DQInOut = (SRAM_WE_NIn == 1'b1) ? memory[SRAM_ADDRIn] : 16'dz;
 
     always @(posedge clk) begin
         if (SRAM_WE_NIn == 1'b0) 
             memory[SRAM_ADDRIn] = SRAM_DQInOut;
-
     end
 endmodule
